cf96885253ab6015b993e48d37cc659cc51e0b9a
lautajam <lautajamdev@gmail.com>
2023-10-13 11:26:37 +0200

    feat: first commit
    This commit is to create and cofigurate project

---
+++ src/main/java/com/lautajam/BazaarAPI/BazaarApiApplication.java
@@ -0,0 +1,13 @@
+package com.lautajam.BazaarAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazaarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazaarApiApplication.class, args);
+	}
+
+}
